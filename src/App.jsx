547c813033b0b36646f1e9906fa67a26fe6c84f8
leebut <lee@leesenglish.com>
2024-01-24 23:33:43 +0800

    Add: new routes and css modules.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import NoRoute from "./pages/NoRoute";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path={"/"} element={<Home />} />
           <Route path={"pricing"} element={<Pricing />} />
           <Route path={"product"} element={<Product />} />
+          <Route path={"app"} element={<AppLayout />} />
+
           <Route path={"*"} element={<NoRoute />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,12 @@
+import styles from "./AppNav.module.css";
+function AppNav() {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>App Navigation</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default AppNav;
